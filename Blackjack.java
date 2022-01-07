42c338ed8a3b3176921cb55662a96052679158fa
John Palenchar <jpalenchar.3@gmail.com>
2022-01-07 08:48:34 -0500

    main updates

--- Blackjack.java
+++ Blackjack.java
@@ -1,2 +1,22 @@
 public class Blackjack {
+    public static void main(String[] args) {
+        // print welcome message
+        System.out.println("START GAME #1");
+        String cunrentMenue = "1";
+        while (isGameDone(cunrentMenue)) {
+        }
+    }
+    public static void loop() {
+            // loop until player has 21 or more
+            // loop until dealer has 21 or more
+            // print winner
+        }
+        public static boolean isGameDone(String value) {
+            // print winner
+            if (value == "quit") {
+                return false;
+            } else {
+                return true;
+            }
+        }
 }
