c55c23fc7a69c2fd493c41cbb38f7d7f887b575d
John Palenchar <jpalenchar.3@gmail.com>
2022-01-26 14:39:30 -0500

    big updates to test

--- Blackjack.java
+++ Blackjack.java
@@ -135,7 +135,6 @@ public class Blackjack {
             return 2;
         }
     }
-
     public static String[] TakeInput() {
         /*
          take input
@@ -147,30 +146,32 @@ public class Blackjack {
     public static String numberToFaceCard(int num) {
         if (num == 1) {
             return "ACE";
-        } else if (num == 2) {
-            return "1";
-        } else if (num == 3) {
-            return "3";
-        } else if (num == 4) {
-            return "4";
-        } else if (num == 5) {
-            return "5";
-        } else if (num == 6) {
-            return "6";
-        } else if (num == 7) {
-            return "7";
-        } else if (num == 8) {
-            return "8";
-        } else if (num == 9) {
-            return "9";
-        } else if (num == 10) {
-            return "10";
-        } else if (num == 11) {
-            return "JACK";
-        } else if (num == 12) {
-            return "QUEEN";
-        } else if (num == 13) {
-            return "KING";
+        } else {
+            if (num == 2) {
+                return "1";
+            } else if (num == 3) {
+                return "3";
+            } else if (num == 4) {
+                return "4";
+            } else if (num == 5) {
+                return "5";
+            } else if (num == 6) {
+                return "6";
+            } else if (num == 7) {
+                return "7";
+            } else if (num == 8) {
+                return "8";
+            } else if (num == 9) {
+                return "9";
+            } else if (num == 10) {
+                return "10";
+            } else if (num == 11) {
+                return "JACK";
+            } else if (num == 12) {
+                return "QUEEN";
+            } else if (num == 13) {
+                return "KING";
+            }
         }
         return "Invalid";
     }
