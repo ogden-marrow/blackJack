5f9126e6f3d88922ca4186b236aa99d87b76de9e
John Palenchar <jpalenchar.3@gmail.com>
2022-01-26 17:30:28 -0500

    total rebuild

--- Blackjack.java
+++ Blackjack.java
@@ -4,10 +4,19 @@ import static java.lang.System.*;
 import static java.lang.System.exit;
 
 class GameStats {
-    double wins;
-    double losses;
-    double ties;
-    double totalGames;
+    int wins;
+    int losses;
+    int ties;
+    int totalGames;
+    double winPercentage = winPercent();
+
+    private int winPercent() {
+        if (totalGames == 0) {
+            return 0;
+        } else {
+            return (wins / totalGames) * 100;
+        }
+    }
 }
 
 public class Blackjack {
@@ -34,29 +43,23 @@ public class Blackjack {
                     out.println("Your hand is: " + Arrays.stream(hand).parallel().reduce(0, Integer::sum) + "\n");
                     break;
                 case "2":
-                    for (int i = 0; i < hand.length; i++) {
-                        if (hand[i] <= 11) {
-                            hand[i] = 10;
-                        }
-                    }
-                    int handSum = Arrays.stream(hand).parallel().reduce(0, Integer::sum); // this mess is to get the sum of the hand
-                    int DHandTotal = rng.nextInt(11) + 16; // this is the dealers hand total
-                    out.println("Dealer's hand: " + DHandTotal);
-                    out.println("Your hand is: " + handSum); // this is the players hand total
-                    if (handSum == 21) { // this is the player winning condition FOR SOME REASON THIS DOES NOT WORK
-                        out.print("You win!\n");
-                    } else if (DHandTotal > 21) { // this is the dealer winning condition
-                        out.println("Dealer wins!\n");
-                    } else if (handSum > 21) { // this is the player winning condition FOR SOME REASON THIS DOES NOT WORK
-                        out.println("Dealer wins!\n");
-                    } else if (handSum > DHandTotal) { // this is the player winning condition FOR SOME REASON THIS DOES NOT WORK
-                        out.print("You win!\n");
-                    } else if (handSum < DHandTotal) { // this is the dealer winning condition
-                        out.println("Dealer wins!\n");
-                    } else { // this is the tie condition
-                        out.println("It's a tie! No one wins!\n");
+                    winFlag = HoldHand(hand);
+                    switch (winFlag) {
+                        case 0:
+                            gameStats.losses++;
+                            break;
+                        case 1:
+                            gameStats.wins++;
+                            break;
+                        case 2:
+                            gameStats.ties++;
+                            break;
+                        default:
+                            out.println("Something went wrong. Please try again.");
+                            break;
                     }
                     out.println("START GAME #" + (gameStats.totalGames + 1) + "\n");
+                    gameStats.totalGames++;
                     break;
                 case "3":
                     PrintStatistics(gameStats);
@@ -84,7 +87,7 @@ public class Blackjack {
          * This function is completed and does not need to be modified.
          */
         out.println("START GAME #1\n");
-        out.println("Your card is a " + hand[0] + "!");
+        out.println("Your card is a " + numberToFaceCard(hand[0]) + "!");
         out.println("Your hand is: " + Arrays.stream(hand).parallel().reduce(0, Integer::sum) + "\n");
     }
 
@@ -114,45 +117,11 @@ public class Blackjack {
         out.println("Number of Dealer wins: " + gameStats.losses);
         out.println("Number of tie games: " + gameStats.ties);
         out.println("Total # of games played is: " + gameStats.totalGames);
-        out.println("Percentage of Player wins: " + (gameStats.wins / gameStats.totalGames) * 100 + "%");
+        out.println("Percentage of Player wins: " + gameStats.winPercentage + "%");
     }
 
     public static int HoldHand(int[] hand) {
-        /*
-         * hold hand
-         * This function needs to be completed. this function still needs the 21 rule
-         * and text add to it.
-         * !!!!! FOR SOME REASON THE HAND DOES NOT WORK WITH THE 21 RULE.
-         */
-        P1Random rng = new P1Random();
-        for (int i = 0; i < hand.length; i++) {
-            if (hand[i] <= 11) {
-                hand[i] = 10;
-            }
-        }
-        int handSum = Arrays.stream(hand).parallel().reduce(0, Integer::sum); // this mess is to get the sum of the hand
-        int DHandTotal = rng.nextInt(11) + 16; // this is the dealers hand total
-        out.println("Dealer's hand: " + DHandTotal);
-        out.println("Your hand is: " + handSum); // this is the players hand total
-        if (handSum == 21) { // this is the player winning condition FOR SOME REASON THIS DOES NOT WORK
-            out.print("You win!\n");
-            return 0;
-        } else if (DHandTotal > 21) { // this is the dealer winning condition
-            out.println("Dealer wins!\n");
-            return 1;
-        } else if (handSum > 21) { // this is the player winning condition FOR SOME REASON THIS DOES NOT WORK
-            out.println("Dealer wins!\n");
-            return 1;
-        } else if (handSum > DHandTotal) { // this is the player winning condition FOR SOME REASON THIS DOES NOT WORK
-            out.print("You win!\n");
-            return 0;
-        } else if (handSum < DHandTotal) { // this is the dealer winning condition
-            out.println("Dealer wins!\n");
-            return 1;
-        } else { // this is the tie condition
-            out.println("It's a tie! No one wins!\n");
-            return 2;
-        }
+        return 0;
     }
 
     public static String[] TakeInput() {
@@ -200,6 +169,7 @@ public class Blackjack {
     }
 
     public static void Exit() {
+
         /*
          * exit
          * This function is completed and does not need to be modified.
